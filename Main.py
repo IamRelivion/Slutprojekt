c4f01076317c7a295ebb4a18d5c2b858fcfc3041
William <Williamsoderstrom04@gmail.com>
2026-05-04 16:09:36 +0200

    Skapande av variabler för rörelse

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import pygame
+import random
 pygame.init() #initialiserar pygame
 
-screen=pygame.display.set_mode((500,500)) #Skapar ett fönster med storleken 500x500
+screen=pygame.display.set_mode((800,600)) #Skapar ett fönster med storleken 500x500
 pygame.display.set_caption("Snake") #Sätter en titel på fönstret
 
 running=True
@@ -11,7 +12,6 @@ while running:#Skapar en loop som gör att programmet inte stängs ned direkt
             running=False
 
     screen.fill((255,255,255))#Fyller i fönstret med vald RGB färg
-    snake=pygame.draw.rect(screen,(0,255,120),pygame.Rect(230,225,25,25))#Ritar en rectangel på skärmen som är Grön
     pygame.display.flip()#updatear hela skärmen istället för bara en del av den
 
 pygame.quit()
@@ -22,15 +22,47 @@ class GameObject():#Skapar en klass som heter GameObject
         self.rect=pygame.rect(x,y,bredd,höjd)#Definerar variablerna
 
 
+
+
 class Snake(GameObject): #skapar en subklass för ormen som ärver av GameObject
     def __init__(self,x,y):
         super.__init__(self,x,y,50,50)#Gör så att du ärver värdena från Parentclassen
-        self.color="Green"
-
-    def move(self,dx,dy): # metod som definierar rörelse för ormen
-        self.rect.x+=dx
-        self.rect.y+=dy
+        self.color=pygame.Color(0,255,12)#Ritar en rectangel på skärmen som är Grön med en tint av blå
+        self.direction=(0,0)#Bestämmer start variablerna för ormens riktning
+        self.expansion=[[x,y]]#Skapar variabler för att växa ormen genom en lista
+        self.grew=False #Skapar en Bool som kommer användas för att se till att ormen inte växer om inte kriterierna är uppnådda
     
-    def draw(self,)
+    def move(self): #metod som definierar rörelse för ormen
+        dx,dy=self.direction # förändring i x och y led 
+        headX,headY=self.expansion[0] # huvudet har index 0 (längst fram)
+        NewHead=[dx+headX,dy+headY] # "nya" huvudet är förändringen i x och y led + huvudet
+        self.expansion.insert(0,NewHead) # sätter in "nya" huvudet i kroppen 
+
+        if self.grew==False:
+            self.expansion.pop()#Tar bort bloket bakom sig om den inte växt
+        else:
+            self.grew==False
+
+
+    
+    
+    def grow(self):
+        self.grew==True
+    
+    
+class Game:
+  def key_input(self):#Skapar en funktion med en dictionary av key inputs
+     keys={
+        pygame.K_UP:(0,-1),
+        pygame.K_DOWN:(0,1),
+        pygame.K_LEFT:(-1,0),
+        pygame.K_RIGHT:(1,0),
+        pygame.K_w:(0,-1),
+        pygame.K_s:(0,1),
+        pygame.K_a:(-1,0),
+        pygame.K_d:(1,0)
+            }
         
-        
+for event in pygame.get():
+    if event.type == pygame.KEYDOWN and event.key in keys:
+        self.snake(keys[event.key])
